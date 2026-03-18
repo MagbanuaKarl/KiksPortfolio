df723d2e0368547e7cb673b25596852e8ad2709f
CarlMagbanua <karlcaniel23@gmail.com>
2026-03-18 15:11:01 +0800

    fix: move client logic to ProjectsPageClient

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 import { getPublicProjects } from "@/lib/publicProjects.server";
 import { ProjectsPageClient } from "@/components/projects/ProjectsPageClient";
 
-
 export default async function ProjectsPage() {  
   const projects = (await getPublicProjects()).sort(
     (a, b) => Number(b.featured) - Number(a.featured)
